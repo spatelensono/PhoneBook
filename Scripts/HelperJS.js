673dcd474bb52ba4eda31005ac7f330d9f0c48e4
spatelensono <spatel@Acxiom.com>
2016-05-03 10:35:06 -0500

    Add Feature Complete 05/03/2016

--- Scripts/HelperJS.js
+++ Scripts/HelperJS.js
@@ -4,6 +4,10 @@ var myPBApp = angular
             .module("myPBModule", ["ngRoute"])
             .config(function ($routeProvider, $locationProvider) {
                 $routeProvider
+                    .when("/Home", {
+                        templateUrl: "/Template/Home.html",
+                        controller: "homeController as homeCtrl"
+                    })
                     .when("/ContactDetails/:id", {
                         templateUrl: "/Template/ContactDetails.html",
                         controller: "viewController as viewCtrl"
@@ -13,24 +17,31 @@ var myPBApp = angular
                         controller: "addNewController as addNewCtrl"
                     })
                     .when("/EditContactDetails/:id", {
-                        templateUrl: "/Template/EditContactDetails.html"
-                        //controller: "editController as editCtrl"
+                        templateUrl: "/Template/EditContactDetails.html",
+                        controller: "editController as editCtrl"
                     })
                     .when("/SaveContactDetails", {
                         templateUrl: "/Template/SaveContactDetails.html",
                         controller: "saveController as saveCtrl"
                     })
                     .otherwise({
-                        redirectTo: "/"
+                        redirectTo: "/Home"
                     })
                 $locationProvider.html5Mode(true);
             })
-            .controller("myPHBController", function ($http, $route, $rootScope) {
+            .controller("homeController", function ($http, $route, $rootScope) {
                 var viewModel = this;
                 //Load data from seed.json
-                $http.get("Data/seed.json").success(function ($data) {
+                if (angular.isUndefined($rootScope.contacts)) {
+                    //alert("contacts is Undefined ");
+                    $http.get("Data/seed.json").success(function ($data) {
                     $rootScope.contacts = $data.contacts;
                 })
+                }
+                else{
+                    //alert("else found");
+                }
+                
             })
             .controller("viewController", function ($routeParams, $rootScope) {
                 //var viewModel = this;
@@ -45,28 +56,55 @@ var myPBApp = angular
                         //viewModel.foundIt = "Not Found";
                     }
                 }
+                //alert($rootScope.tmpContact.avatar);
             })
             .controller("addNewController", function ($rootScope) {
                 var viewModel = this;
                 var addContact = new Object();
                 addContact.id = $rootScope.contacts[$rootScope.contacts.length -1].id + 1;
-                addContact.name ="FName LName";
-                addContact.phone = "(987) 654-3210";
-                addContact.email = "email@domain.com";
-                addContact.birthday = "01/01/1980";
-                addContact.avatar = "http://avtar/na/avatari";
+                addContact.name ="";
+                addContact.phone = "";
+                addContact.email = "";
+                addContact.birthday = "";
+                addContact.avatar = "";
                 $rootScope.addNewContact = addContact;
                 //alert($rootScope.addNewContact.id);
             })
-            .controller("saveController", function ($rootScope) {
+            .controller("saveController", function ($location, $rootScope) {
                 var viewModel = this;
-               $rootScope.contacts.push({
-                    "id": $rootScope.addNewContact.id,
-                    "name": $rootScope.addNewContact.name,
-                    "phone": $rootScope.addNewContact.phone,
-                    "email": $rootScope.addNewContact.email,
-                    "birthday": $rootScope.addNewContact.birthday,
-                    "avatar": $rootScope.addNewContact.avatar
-               });
-               //alert($rootScope.addNewContact.id);
+                if($rootScope.addNewContact.name == "") {
+                    alert("Name Not Enter");
+                    $location.path("/AddNewContact");
+                }
+                else if ($rootScope.addNewContact.phone == "") {
+                    alert("Phone Not Enter");
+                    $location.path("/AddNewContact");
+                }
+                else if ($rootScope.addNewContact.email == "") {
+                    alert("Email Not Enter");
+                    $location.path("/AddNewContact");
+                }
+                else if ($rootScope.addNewContact.birthday == "") {
+                    alert("Birthday Not Enter");
+                    $location.path("/AddNewContact");
+                }
+                else if ($rootScope.addNewContact.avatar == "") {
+                    alert("Avatar link Not Enter");
+                    $location.path("/AddNewContact");
+                }
+                else {
+                    $rootScope.contacts.push({
+                        "id": $rootScope.addNewContact.id,
+                        "name": $rootScope.addNewContact.name,
+                        "phone": $rootScope.addNewContact.phone,
+                        "email": $rootScope.addNewContact.email,
+                        "birthday": $rootScope.addNewContact.birthday,
+                        "avatar": $rootScope.addNewContact.avatar
+                   });
+                    $location.path("/Home");
+                }
+
+            })
+            .controller("editController", function ($rootScope) {
+                //alert($rootScope.tmpContact.name);
             })
